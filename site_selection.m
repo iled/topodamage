25bd46ad20b7044668f889ba7c7fd7ac440d0528
Júlio <drawzal@yahoo.com>
2017-11-05 22:33:13 -0500

    compute and plot percentile analysis
    using new age of housing tiff.
    using the helper function.
    the plots still need some adjusting.
    confirm drainage density map.

--- site_selection.m
+++ site_selection.m
@@ -6,7 +6,7 @@ drainage_density = GRIDobj('resources/drainage_density.tif');
 slope = GRIDobj('resources/slope_gauss.tif');
 wetness_index = GRIDobj('resources/wetness_index.tif');
 % non-topographic metrics
-housing_age = GRIDobj('resources/age_houses_PointToRaster_Cli71.tif');
+housing_age = GRIDobj('resources/agehouses_tmp3.tif');
 canopy  = GRIDobj('resources/canopyclipped.tif');
 impervious = GRIDobj('resources/impervious_final.tif');
 soil = GRIDobj('resources/soils_final.tif');
@@ -14,6 +14,8 @@ soil = GRIDobj('resources/soils_final.tif');
 % fix impervious map
 impervious.refmat = DEM.refmat;
 impervious.cellsize = DEM.cellsize;
+% fix age of housing
+housing_age.Z(housing_age.Z < 1755) = NaN;
 
 %% compute averages over 3 standard city blocks (330 m x 330 m)
 DEM_avg = DEM;
@@ -48,7 +50,27 @@ for i = 1:numel(grids)
 end
 
 %% plot age of housing distribution
-histogram(housing_age.Z(housing_age.Z > 0))
+histogram(housing_age_avg.Z)
 
-%% analyze slope, fix others
+%% analyze each one of the topographic metrics, fix non-topographic
+topo = {drainage_area_avg, drainage_density_avg, slope_avg, wetness_index_avg};
+nontopo = {housing_age, canopy_avg, impervious_avg, soil_avg}; % TODO: fix and use housing average
+locations = cell(1, numel(topo));
+for i = 1:numel(topo)
+    [xl, yl, xh, yh] = perc_analysis(topo{i}, nontopo);
+    locations{i} = {xl, yl, xh, yh};
+end
+%% plot each of the topographic metrics, with fixed non-topographic
+close all
 
+for i = 1:numel(topo)
+    subplot(2, 2, i)
+    %imageschs(DEM)
+    imagesc(topo{i});
+    hold on
+    coord = locations{i};
+    plot(coord{1}, coord{2}, 'ok', coord{3}, coord{4},'sr')
+    %imagesc(topo{i})
+    %title();
+    hold off
+end
