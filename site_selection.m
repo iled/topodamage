3e0b62e1afd27067f9ee71a6e90c634a1d37bce1
Júlio <drawzal@yahoo.com>
2017-11-06 21:03:14 -0500

    add special filters for drainage density and housing age
    . drainage density filtered with max filter 5x5
    . housing age filtered with min filter 5x5
    . load and use fixed drainage density
    . load simplified soil
    . remove deprecated housing age maps

--- site_selection.m
+++ site_selection.m
@@ -2,7 +2,7 @@
 % topographic metrics
 DEM = GRIDobj('resources/DEM30_gauss_filled.tif');
 drainage_area = GRIDobj('resources/drainage_area_mdf.tif');
-drainage_density = GRIDobj('resources/drainage_density.tif');
+drainage_density = GRIDobj('resources/drainage_density_fixed.tif');
 slope = GRIDobj('resources/slope_gauss.tif');
 wetness_index = GRIDobj('resources/wetness_index.tif');
 % non-topographic metrics
@@ -17,13 +17,19 @@ impervious.cellsize = DEM.cellsize;
 % fix age of housing
 housing_age.Z(housing_age.Z < 1755) = NaN;
 
+%% simplified soil
+soil2 = GRIDobj('resources/soils_color_number_codes_Pol51.tif');
+imagesc(soil2)
+%%
+histogram(soil2.Z, 'Normalization', 'probability')
+
 %% compute averages over 3 standard city blocks (330 m x 330 m)
 DEM_avg = DEM;
 drainage_area_avg = drainage_area;
-drainage_density_avg = drainage_density;
+% drainage_density_avg = drainage_density;
 slope_avg = slope;
 wetness_index_avg = wetness_index;
-housing_age_avg = housing_age;
+% housing_age_avg = housing_age;
 canopy_avg = canopy;
 impervious_avg = impervious;
 soil_avg = soil;
@@ -31,9 +37,9 @@ soil_avg = soil;
 n_px = 11;
 h = ones(n_px, n_px) / n_px ^ 2;
 
-grids = {DEM, drainage_area, drainage_density, slope, wetness_index, ...
-    housing_age, canopy, impervious, soil};
-grids_avg = {'DEM_avg', 'drainage_area_avg', 'drainage_density_avg', 'slope_avg', 'wetness_index_avg', ...
+grids = {DEM, drainage_area, slope, wetness_index, ...
+    canopy, impervious, soil};
+grids_avg = {'DEM_avg', 'drainage_area_avg', 'slope_avg', 'wetness_index_avg', ...
     'housing_age_avg', 'canopy_avg', 'impervious_avg', 'soil_avg'};
 
 for i = 1:numel(grids)
@@ -41,6 +47,33 @@ for i = 1:numel(grids)
     tempval.Z = imfilter(grids{i}.Z, h);
     assignin('base', grids_avg{i}, tempval);
 end
+
+%% specific filter for drainage density: max filter with edge n_px
+n_px = 5;
+drainage_density_avg = drainage_density;
+drainage_density_avg.Z = fillmissing(drainage_density_avg.Z, 'constant', -1);
+drainage_density_avg.Z = ordfilt2(drainage_density_avg.Z, n_px ^ 2, ones(n_px, n_px));
+drainage_density_avg.Z(drainage_density_avg.Z == -1) = NaN;
+%% plot original drainage density vs filtered
+subplot(1, 2, 1)
+imagesc(drainage_density), colorbar
+subplot(1, 2, 2)
+imagesc(drainage_density_avg), colorbar
+
+%% specific filter for age of housing: min filter with edge n_px
+n_px = 5;
+housing_age_avg = housing_age;
+housing_age_avg.Z = fillmissing(housing_age_avg.Z, 'constant', 9999);
+housing_age_avg.Z = ordfilt2(housing_age_avg.Z, 1, ones(n_px, n_px));
+housing_age_avg.Z(housing_age_avg.Z == 9999) = NaN;
+housing_age_avg.Z(housing_age_avg.Z == 0) = NaN;
+%% plot original drainage density vs filtered
+subplot(1, 2, 1)
+imagesc(housing_age), colorbar
+subplot(1, 2, 2)
+imagesc(housing_age_avg), colorbar
+%%
+print('age_houses_minfilter5x5', '-djpeg', '-r300')
 
 %% plot the averaged grids
 for i = 1:numel(grids)
@@ -83,3 +116,6 @@ for i = 1:numel(topo)
     title(topo_title{i});
     hold off
 end
+
+%% save plot
+print('perc_analysis', '-djpeg', '-r300')
