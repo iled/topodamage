6b30925c589bb9dc8fa6c124ef8f638e33cb3461
Arielle <woods.arielle@gmail.com>
2017-11-08 13:52:58 -0500

    coordinates

---
+++ coordinates.m
@@ -0,0 +1,6 @@
+%% row 1 is high, row 2 is low
+drainage_area_coords = [585181.1115, 4477942.9546; 582121.5172, 4474555.5467]
+slope_coords = [589278.7824, 4484034.8252; 589907.092, 4483843.6007]
+drainage_density_coords = [583816.9, 4480538.1; 583350.8, 4480483.5]
+save('coordinates', 'drainage_area', 'slope_coords', 'drainage_density_coords');
+
